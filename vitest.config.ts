d3656beaff8559b33b1959f18dc0a1914eb4745e
Bernd Matzner <bernd.matzner@regard.io>
2025-12-21 23:48:36 +0100

    fix: extending dev configs

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,6 @@
+import { vitestNodeConfig } from '@regardio/dev/vitest/node';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    environment: 'node',
-    exclude: ['node_modules', 'dist'],
-    globals: true,
-    include: ['**/*.test.ts', '**/*.test.tsx'],
-  },
+  test: vitestNodeConfig,
 });
